70b92133259d31ee1f2449c0c3674e9182bab44d
Surve <surve8729@gmail.com>
2024-05-20 16:38:49 +0530

    Add haptic feedback to button clicks

--- app/src/main/java/com/example/dentaku/MainActivity.kt
+++ app/src/main/java/com/example/dentaku/MainActivity.kt
@@ -10,18 +10,17 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
+import android.os.Vibrator
+import android.content.Context
 
 class MainActivity : AppCompatActivity() {
-    var textview1:TextView ?= null
-    var lastNumeric:Boolean = false
-    var lastDot:Boolean = false
+    var textview1: TextView? = null
+    var lastNumeric: Boolean = false
+    var lastDot: Boolean = false
 
-    //hello
-
+    private lateinit var vibrator: Vibrator
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -44,175 +43,174 @@ class MainActivity : AppCompatActivity() {
         val btnDivide = findViewById<Button>(R.id.bt_divide)
         val btnEqual = findViewById<Button>(R.id.btn_equal)
 
+        vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
         val mediaPlayer = MediaPlayer.create(this, R.raw.retrosound)
         val mediaPlayerClr = MediaPlayer.create(this, R.raw.clr)
-        var mediaPlayerEqual = MediaPlayer.create(this, R.raw.equal)
+        val mediaPlayerEqual = MediaPlayer.create(this, R.raw.equal)
         val mediaPlayerOperator = MediaPlayer.create(this, R.raw.operator)
 
 
         //setting onclick listener
-        bt1.setOnClickListener(){
+        bt1.setOnClickListener {
             txt_append(1)
             lastNumeric = true
             lastDot = false
-            mediaPlayer?.start()
+            mediaPlayer.start()
+            vibrator.vibrate(1) // Vibrate for 5 milliseconds
         }
 
-        bt2.setOnClickListener(){
+        bt2.setOnClickListener {
             txt_append(2)
             lastNumeric = true
             lastDot = false
-            mediaPlayer?.start()
+            mediaPlayer.start()
+            vibrator.vibrate(1)
         }
 
-        bt3.setOnClickListener(){
+        bt3.setOnClickListener {
             txt_append(3)
             lastNumeric = true
             lastDot = false
-            mediaPlayer?.start()
+            mediaPlayer.start()
+            vibrator.vibrate(1)
         }
 
-        bt4.setOnClickListener(){
+        bt4.setOnClickListener {
             txt_append(4)
             lastNumeric = true
             lastDot = false
-            mediaPlayer?.start()
+            mediaPlayer.start()
+            vibrator.vibrate(1)
         }
 
-        bt5.setOnClickListener(){
+        bt5.setOnClickListener {
             txt_append(5)
             lastNumeric = true
             lastDot = false
-            mediaPlayer?.start()
+            mediaPlayer.start()
+            vibrator.vibrate(1)
         }
 
-        bt6.setOnClickListener(){
+        bt6.setOnClickListener {
             txt_append(6)
             lastNumeric = true
             lastDot = false
-            mediaPlayer?.start()
+            mediaPlayer.start()
+            vibrator.vibrate(1)
         }
 
-        bt7.setOnClickListener(){
+        bt7.setOnClickListener {
             txt_append(7)
             lastNumeric = true
             lastDot = false
-            mediaPlayer?.start()
+            mediaPlayer.start()
+            vibrator.vibrate(1)
         }
 
-        bt8.setOnClickListener(){
+        bt8.setOnClickListener {
             txt_append(8)
             lastNumeric = true
             lastDot = false
-            mediaPlayer?.start()
+            mediaPlayer.start()
+            vibrator.vibrate(1)
         }
 
-        bt9.setOnClickListener(){
+        bt9.setOnClickListener {
             txt_append(9)
             lastNumeric = true
             lastDot = false
-            mediaPlayer?.start()
+            mediaPlayer.start()
+            vibrator.vibrate(1)
         }
 
-        bt0.setOnClickListener(){
+        bt0.setOnClickListener {
             txt_append(0)
             lastNumeric = true
             lastDot = false
-            mediaPlayer?.start()
+            mediaPlayer.start()
+            vibrator.vibrate(1)
         }
 
-        btdot.setOnClickListener(){
+        btdot.setOnClickListener {
             decimal()
             lastNumeric = false
             lastDot = true
-            mediaPlayerOperator?.start()
+            mediaPlayerOperator.start()
+            vibrator.vibrate(3)
         }
 
-        btnPlus.setOnClickListener(){
+        btnPlus.setOnClickListener {
             onOperator("+")
-            mediaPlayerOperator?.start()
+            mediaPlayerOperator.start()
+            vibrator.vibrate(3)
         }
 
-        btnDivide.setOnClickListener(){
+        btnDivide.setOnClickListener {
             onOperator("/")
-            mediaPlayerOperator?.start()
-
+            mediaPlayerOperator.start()
+            vibrator.vibrate(3)
         }
 
-        btnMuliply.setOnClickListener(){
+        btnMuliply.setOnClickListener {
             onOperator("x")
-            mediaPlayerOperator?.start()
-
+            mediaPlayerOperator.start()
+            vibrator.vibrate(3)
         }
 
-        btnMinus.setOnClickListener(){
+        btnMinus.setOnClickListener {
             onOperator("-")
-            mediaPlayerOperator?.start()
-
+            mediaPlayerOperator.start()
+            vibrator.vibrate(3)
         }
 
-        btnEqual.setOnClickListener(){
+        btnEqual.setOnClickListener {
             onEqual("=")
-
-
+            vibrator.vibrate(8)
         }
 
-
-        btnClr.setOnClickListener(){
+        btnClr.setOnClickListener {
             clear()
-            mediaPlayerClr?.start()
-
+            mediaPlayerClr.start()
+            vibrator.vibrate(30)
         }
-
-
-
     }
 
-    private fun txt_append(num:Int){
-       // var textview1 = findViewById<TextView>(R.id.textview1)
+    private fun txt_append(num: Int) {
         textview1?.append(num.toString())
-
     }
 
-    private fun clear(){
-       // var textview1 = findViewById<TextView>(R.id.textview1)
-        textview1?.text=""
+    private fun clear() {
+        textview1?.text = ""
     }
 
-    fun decimal(){
-        if (lastNumeric && !lastDot){
-           // var textview1 = findViewById<TextView>(R.id.textview1)
+    private fun decimal() {
+        if (lastNumeric && !lastDot) {
             textview1?.append(".")
             lastNumeric = false
             lastDot = true
         }
     }
 
-
-    //Arithmetic operations
-
-    private fun onOperator(view: String){
+    private fun onOperator(view: String) {
         textview1?.text?.let {
-            if(lastNumeric && !isOperatorAdd(it.toString())){
-                textview1?.append((view))
+            if (lastNumeric && !isOperatorAdded(it.toString())) {
+                textview1?.append(view)
                 lastNumeric = false
                 lastDot = false
             }
         }
     }
 
-    fun isOperatorAdd(value: String):Boolean{
-        return if (value.startsWith("-")){
+    private fun isOperatorAdded(value: String): Boolean {
+        return if (value.startsWith("-")) {
             false
-        }else{
-            value.contains("/")
-                    || value.contains("x")
-                    || value.contains("+")
-                    || value.contains("-")
+        } else {
+            value.contains("/") || value.contains("x") || value.contains("+") || value.contains("-")
         }
     }
 
-    fun onEqual(view: String){
+    private fun onEqual(view: String) {
         val mediaPlayerOppai = MediaPlayer.create(this, R.raw.oppai)
         val mediaPlayerClear = MediaPlayer.create(this, R.raw.clr)
         val mediaPlayerEqual = MediaPlayer.create(this, R.raw.equal)
@@ -220,12 +218,10 @@ class MainActivity : AppCompatActivity() {
         var tvValue = textview1?.text.toString()
         var prefix = ""
         try {
-
-
             //Minus
-            if (tvValue.startsWith("-")){
+            if (tvValue.startsWith("-")) {
                 prefix = "-"
-                tvValue = tvValue.substring((1))
+                tvValue = tvValue.substring(1)
             }
             if (tvValue.contains("-")) {
                 val splitValue = tvValue.split("-")
@@ -233,20 +229,18 @@ class MainActivity : AppCompatActivity() {
                 var one = splitValue[0]
                 var two = splitValue[1]
 
-                if (prefix.isNotEmpty()){
+                if (prefix.isNotEmpty()) {
                     one = prefix + one
                 }
 
-                var result = one.toDouble() - two.toDouble()
-
-                textview1?.text = (result).toString()
-                if (result == 106.0){
-                    mediaPlayerOppai?.start()
-                }else{
+                val result = one.toDouble() - two.toDouble()
+                textview1?.text = result.toString()
+                if (result == 106.0) {
+                    mediaPlayerOppai.start()
+                } else {
                     mediaPlayerEqual.start()
                 }
             }
-
 
             //Plus
             else if (tvValue.contains("+")) {
@@ -255,14 +249,13 @@ class MainActivity : AppCompatActivity() {
                 var one = splitValue[0]
                 var two = splitValue[1]
 
-                if (prefix.isNotEmpty()){
+                if (prefix.isNotEmpty()) {
                     one = prefix + one
                 }
 
                 textview1?.text = (one.toDouble() + two.toDouble()).toString()
                 mediaPlayerEqual.start()
             }
-
 
             //Divide
             else if (tvValue.contains("/")) {
@@ -271,14 +264,13 @@ class MainActivity : AppCompatActivity() {
                 var one = splitValue[0]
                 var two = splitValue[1]
 
-                if (prefix.isNotEmpty()){
+                if (prefix.isNotEmpty()) {
                     one = prefix + one
                 }
 
                 textview1?.text = (one.toDouble() / two.toDouble()).toString()
                 mediaPlayerEqual.start()
             }
-
 
             //Multiply
             else if (tvValue.contains("x")) {
@@ -287,21 +279,16 @@ class MainActivity : AppCompatActivity() {
                 var one = splitValue[0]
                 var two = splitValue[1]
 
-                if (prefix.isNotEmpty()){
+                if (prefix.isNotEmpty()) {
                     one = prefix + one
                 }
 
-                var result = one.toDouble() * two.toDouble()
-
-                textview1?.text = (result).toString()
-
+                val result = one.toDouble() * two.toDouble()
+                textview1?.text = result.toString()
                 mediaPlayerEqual.start()
-
-
-
             }
 
-            // NHENTAI WEBVIEW CLIENT
+            // ------------------- NHENTAI WEBVIEW CLIENT -------------------
 
             val nhentaiBaseUrl = "https://nhentai.to/g/"
             val webView = findViewById<WebView>(R.id.webView)
@@ -316,28 +303,24 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-            val textview1 = findViewById<TextView>(R.id.textview1)
             val btnClear = findViewById<Button>(R.id.btn_clr)
-            val mediaPlayerClear = MediaPlayer.create(this, R.raw.clr)
 
             btnClear.setOnLongClickListener {
-                val textContent = textview1.text.toString()
+                val textContent = textview1?.text.toString()
 
-                if (!textContent.isNullOrBlank() && textContent.any { it.isDigit() }) {
+                if (textContent.isNotBlank() && textContent.any { it.isDigit() }) {
                     // If textContent is not null, not empty, and contains integers, add it to the URL
+                    vibrator.vibrate(10)
                     val nhentaiUrl = nhentaiBaseUrl + textContent
                     webView.loadUrl(nhentaiUrl)
                     webView.visibility = View.VISIBLE
                 } else {
+                    vibrator.vibrate(30)
                     mediaPlayerClear.start()
                 }
                 true
             }
-
-
-
-
-        }catch (e: ArithmeticException){
+        } catch (e: ArithmeticException) {
             e.printStackTrace()
         }
     }
@@ -351,8 +334,4 @@ class MainActivity : AppCompatActivity() {
             webView.visibility = View.GONE
         }
     }
-
-    //Sound one
-
-
 }
